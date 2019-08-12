af899fe694b7b8e1ba03b4c6d4318dff65846cf4
Edward Jose <edward@me.com>
2019-08-12 17:41:32 +0800

    removed celery test

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -1,20 +1,20 @@
-import pytest
+# import pytest
 
-from myapp.app import init_celery
-from myapp.tasks.example import dummy_task
-
-
-@pytest.fixture
-def celery_app(celery_app, app):
-    celery = init_celery(app)
-
-    celery_app.conf = celery.conf
-    celery_app.Task = celery_app.Task
-
-    yield celery_app
+# from myapp.app import init_celery
+# from myapp.tasks.example import dummy_task
 
 
-def test_example(celery_app, celery_worker):
-    """Simply test our dummy task using celery"""
-    res = dummy_task.delay()
-    assert res.get() == "OK"
+# @pytest.fixture
+# def celery_app(celery_app, app):
+#     celery = init_celery(app)
+
+#     celery_app.conf = celery.conf
+#     celery_app.Task = celery_app.Task
+
+#     yield celery_app
+
+
+# def test_example(celery_app, celery_worker):
+#     """Simply test our dummy task using celery"""
+#     res = dummy_task.delay()
+#     assert res.get() == "OK"
